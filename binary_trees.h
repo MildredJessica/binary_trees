af0955f9d34e44931ec47814a1b768ee3057b621
MildredJessica <nwonyejessica@gmail.com>
2023-06-01 19:33:21 +0100

    Removed a root

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -55,6 +56,7 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
+bst_t *min_finder(bst_t *root);
 bst_t *bst_remove(bst_t *root, int value);
 
 #endif
