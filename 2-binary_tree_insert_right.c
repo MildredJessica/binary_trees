14390928e0a71f72377b23654c9b63182fdd9491
MildredJessica <nwonyejessica@gmail.com>
2023-06-02 19:15:44 +0100

    Changed the method for inserting left and right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,20 +9,19 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
-new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL || parent == NULL)
+if (parent == NULL)
 return (NULL);
 
-new_node->n = value;
-new_node->parent = parent;
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+return (NULL);
+
+
 if (parent->right != NULL)
 {
 new_node->right = parent->right;
 parent->right->parent = new_node;
 }
-else
-new_node->right = NULL;
 parent->right = new_node;
-new_node->left = NULL;
 return (new_node);
 }
