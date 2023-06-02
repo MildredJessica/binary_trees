49b6a6d0ba2e85ccfae5cd8a64fcc60f0cd4b2fc
MildredJessica <nwonyejessica@gmail.com>
2023-06-02 19:09:43 +0100

    Changed the method

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_insert_left - Insert to the left of the binary tree
  * @parent: parent of the tree
@@ -10,20 +9,17 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
-new_node = malloc(sizeof(binary_tree_t));
+
+new_node = binary_tree_node(parent, value);
 if (new_node == NULL || parent == NULL)
 return (NULL);
 
-new_node->n = value;
-new_node->parent = parent;
 if (parent->left != NULL)
 {
 new_node->left = parent->left;
 parent->left->parent = new_node;
 }
 else
-new_node->left = NULL;
 parent->left = new_node;
-new_node->right = NULL;
 return (new_node);
 }
